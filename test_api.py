a524a7515989d7c836fdc52387bd7f5df734b61a
Olmekk <jonathansalvador395@gmail.com>
2026-05-28 22:49:55 -0600

    fix url

--- test_api.py
+++ test_api.py
@@ -2,7 +2,8 @@ import requests
 import json
 
 # url local de tu api asegúrate de que el puerto coincida con tu docker
-url = "http://127.0.0.1:8000/predict/anomaly"
+# url = "http://127.0.0.1:8000/predict/anomaly"
+url = "http://100.100.217.46:8000/predict/anomaly"
 
 # encabezados necesarios para la conexion
 headers = {
@@ -13,7 +14,7 @@ headers = {
 # caso 1 datos normales dentro del limite de la tv led
 datos_normales = {
   "device_type": "TV LED",
-  "power_w": 55.0,
+  "power_w": 60.0,
   "voltage": 120.0,
   "current_a": 0.45
 }
